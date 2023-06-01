f77346aba6ccc47eacdbc3a5d5a9c11e4e1df84f
Michael <michael65059@gmail.com>
2023-06-01 16:30:00 +0200

    Finished rock, paper and scissors game

--- rock, paper and scissors game.py
+++ rock, paper and scissors game.py
@@ -4,10 +4,24 @@ while True:
   choices = ['rock', 'paper', 'scissors']
   computer = random.choice(choices)
   player = None
+  beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+  #prevents user from entering wrong input(repeats loop)
   while player not in choices:
-    player = input('Enter: ROCK, PAPER or SCISSORS').lower()
+    player = input('Enter: ROCK, PAPER or SCISSORS:  ').lower()
     
-#just testing github
-
+    
   
+  if beats[player] == computer:
+   print("Computer won!")
+  if beats[computer] == player:
+    print("Player won!")
+  if player == computer:
+    print("Draw!")
+
+  play_again = input('Do you want to try again?: yes  no: ')
+
+  if play_again != 'yes':
+    break
+   
+print('Have a good day!')
 
